776040c4c065f0c0be4f093e5a901f985c1219eb
Jason Furtney <jfurtney@itascacg.com>
2021-11-16 14:54:20 -0600

    plot houses as hollow

--- route.py
+++ route.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib; matplotlib.rcParams["savefig.directory"] = "."
 from matplotlib import pyplot as plt
 import random
-from matplotlib.patches import Polygon
+from matplotlib.patches import Circle, Wedge, Polygon
 from matplotlib.collections import PatchCollection
 import networkx as nx
 
@@ -17,8 +17,7 @@ for i, ((hx, hy), polygon, _) in enumerate(houses):
     yy.append(hy)
     # px, py = closest_road_to_house[i][0]
     #plt.plot([hx,hx],[hy,hy], "o")
-    polygon = Polygon(polygon.T, True, fill=False)
-    patches.append(polygon)
+    plt.plot(*polygon, "-", color="grey")
 
 ih = random.sample(range(len(houses)),2)
 
@@ -32,7 +31,6 @@ for hid in ih:
     G.add_edge(n2,n1, weight=np.linalg.norm(np.array(G.nodes[n1]["pos"])-np.array((rx,ry))))
     G.nodes[n2]["pos"] = [rx, ry]
     plt.plot(hx,hy,"o", color="red")
-    plt.plot(rx,ry,"o", color="red")
     new_nodes.append(n2)
 
 
@@ -40,8 +38,8 @@ for hid in ih:
 
 for n0, n1 in G.edges:
     (s0x, s0y), (s1x, s1y) = np.array(G.nodes[n0]["pos"]), np.array(G.nodes[n1]["pos"])
-    plt.plot([s0x,s1x], [s0y,s1y], "-", color="grey")
-plt.gca().add_collection(PatchCollection(patches))
+    plt.plot([s0x,s1x], [s0y,s1y], "-", linewidth=0.5, color="grey")
+#plt.gca().add_collection(PatchCollection(patches))
 
 path = nx.shortest_path(G, *new_nodes,weight="weight")
 xx,yy = [], []
@@ -49,7 +47,7 @@ for nid in path:
     x,y = G.nodes[nid]["pos"]
     xx.append(x)
     yy.append(y)
-plt.plot(xx,yy,"--", linewidth=3, color="red")
+plt.plot(xx,yy,"-", linewidth=3, color="red")
 
 plt.gca().set_aspect(1)
 plt.show()
