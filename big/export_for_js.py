787a8721da21bf1248cce4afc4b6a7952845fed5
Jason Furtney <jfurtney@itascacg.com>
2021-11-23 09:44:10 -0600

    refactor out unneeded data on all roads

--- big/export_for_js.py
+++ big/export_for_js.py
@@ -5,43 +5,42 @@ import json
 node_hash, house_ways, G, node_node, new_nodes = joblib.load("stpaul_processed2.pkl")
 
 # we could also do some geometric simplification of the segments
-way_nodes = set()
 
-for (n0,n1), data in G.edges.items():
+way_nodes = set()
+for (n0,n1), _ in G.edges.items():
     way_nodes.add(n0)
     way_nodes.add(n1)
 
 way_nodes = list(way_nodes)
-
-old_to_new_map = {_:i for i,_ in enumerate(way_nodes)}
-
-nodes = []
 nx=[]
 ny=[]
 for node in way_nodes:
+    x,y = node_hash[node]
+    nx.append(x)
+    ny.append(y)
+xmin, xmax = min(nx), max(nx)
+ymin, ymax = min(ny), max(ny)
+del way_nodes, nx, ny
+
+
+delivery_nodes = set()
+for (n0, n1), (_, route) in node_node.items():
+    delivery_nodes.add(n0)
+    delivery_nodes.add(n1)
+    for n in route:
+        delivery_nodes.add(n)
+
+delivery_nodes = list(delivery_nodes)
+old_to_new_map = {_:i for i,_ in enumerate(delivery_nodes)}
+nodes = []
+nx=[]
+ny=[]
+for node in delivery_nodes:
     nodes.append(node_hash[node])
     x,y = node_hash[node]
     nx.append(x)
     ny.append(y)
 
-edges = []
-e0, e1 = [], []
-for (n0,n1), data in G.edges.items():
-    m0, m1 = old_to_new_map[n0], old_to_new_map[n1]
-    edges.append((min(m0,m1), max(m0,m1)))
-    e0.append(min(m0,m1))
-    e1.append(max(m0,m1))
-
-x_street = []
-y_street = []
-for i in G.nodes():
-    x,y = node_hash[i]
-    G.nodes[i]["pos"] = x, y
-    x_street.append(x)
-    y_street.append(y)
-
-xmin, xmax = min(x_street), max(x_street)
-ymin, ymax = min(y_street), max(y_street)
 
 deliveries = []
 for n in new_nodes:
@@ -59,8 +58,7 @@ for (n0, n1), data in node_node.items():
     str_key_routes[key] = data[0], new_route
 
 json.dump({"nx": nx, "ny": ny,
-           "e0": e0, "e1": e1,
            "limits": (xmin, xmax, ymin, ymax),
            "new_nodes": deliveries,
            "node_routes": str_key_routes},
-          open("roads.json", "w"))
+          open("routes.json", "w"))
