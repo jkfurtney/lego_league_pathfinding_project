53ca33bb57c38ba7d23974e3dd099bf1539e4b60
Jason Furtney <jfurtney@itascacg.com>
2021-11-28 15:42:27 -0600

    adding algorithm solutions to page

--- big/make_rounds.py
+++ big/make_rounds.py
@@ -5,6 +5,7 @@ template = Path('template.html').read_text()
 cases = [
     ("(Round 1)", "_round_123321_10.json"),
     ("(Round 2)", "_round_123321_20.json"),
+    ("(Round 2.5)", "_routes.json"),
     ("(Round 3)", "_round_123321_40.json"),
     ("(Round 4)", "_round_123321_50.json"),
     ("(Round 5)", "_round_123321_60.json")]
@@ -12,5 +13,5 @@ cases = [
 for name, datafile in cases:
     parameters = {"round": name, "datafile": datafile}
     new_file = jinja2.Environment().from_string(template).render(parameters)
-    with open(datafile[:-5]+".html", "w") as f:
+    with open(datafile[1:-5]+".html", "w") as f:
         print(new_file, file=f)
