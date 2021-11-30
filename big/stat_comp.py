6ec52945569b1594da4b622cdac6fe96a159cfe2
Jason Furtney <jfurtney@itascacg.com>
2021-11-30 14:50:06 -0600

    print the percent of times each algorithm is the fastest

--- big/stat_comp.py
+++ big/stat_comp.py
@@ -1,10 +1,11 @@
 import numpy as np
 import joblib
 import math
-
+from collections import defaultdict, Counter
 data=joblib.load("comp_res.pkl")
 
 alg_names = "nx_christofides", "nx_greedy", "random_path", "deliberately_bad", "google_generic_tabu_search", "google_greedy_descent", "google_guided_local_search", "google_simulated_annealing", "google_tabu_search",
+
 
 
 for key, d in data.items():
@@ -14,8 +15,10 @@ for key, d in data.items():
     frac = (d.T/case_min).T
     mean_score = (frac.mean(axis=0)-1)*100
     order = np.argsort(mean_score)
+    best = defaultdict(int)
+    c = Counter(d.argmin(axis=1))
     for i in order:
         score = mean_score[i]
         # if score==0: score="best"
         # else: score=math.log10(score)
-        print(alg_names[i], f"{score:.2f}", "%")
+        print(alg_names[i], 100*c[i]/len(d),f"{score:.2f}", "%")
